797d7797357576adfeb9def495319f403fb6e62b
Vladislav Denisov <denisov076@gmail.com>
2021-12-08 23:56:30 +0300

    set color fo 2 views

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewSetting()
+        buttonSettings()
+
     }
 
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        startButton.setTitle("Next", for: .normal)
+        changeColor()
+        
 
+    }
+    
+    
+    func viewSetting() {
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+    }
+    
+    func buttonSettings() {
+        startButton.backgroundColor = .blue
+        startButton.tintColor = .white
+        startButton.layer.cornerRadius = 10
+        startButton.setTitle("Start", for: .normal)
+    }
+    
+    func changeColor() {
+        if redView.alpha != 1  {
+            redView.alpha = 1
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+        } else if yellowView.alpha != 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            greenView.alpha = 0.3
+        } else if greenView.alpha != 1 {
+            greenView.alpha = 1
+        }
+    
+}
 }
 
